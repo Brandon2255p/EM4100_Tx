68d39111b6202f8a31fbf22167df9e0484d42655
brandon <brandon2255p@gmail.com>
2017-06-25 09:30:34 +0200

    initial commit

---
+++ main/main.ino
@@ -0,0 +1,27 @@
+/*
+ * Blink
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+
+#include <Arduino.h>
+#include "Manchester.h"
+
+int TX_PIN = 9;
+
+uint8_t tagGarage[5] = {0x30, 0x7E, 0xA4, 0x53, 0x5A};
+uint8_t tagWork[5] = {0x30, 0x7E, 0xA1, 0x03, 0x92};
+uint8_t tagUCT[5] = {0x30, 0x7E, 0xA4, 0x53, 0x5A};
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(TX_PIN, OUTPUT);
+  man.setupTransmit(TX_PIN, MAN_4800);
+}
+
+void loop()
+{
+  man.transmitEM4100(tagWork);
+  delay(500);
+}
