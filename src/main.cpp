d7a397eeca37e2e34de298261198f9a08dd860bb
Brandon Piner <brandon2255p@gmail.com>
2017-12-08 23:05:14 +0200

    Added attiny platform Cleaned code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,9 @@
 #include <Arduino.h>
 #include "Manchester.h"
 
-int TX_PIN = 9;
+int TX_PIN = 2;
 
-uint8_t tagGarage[5] = {0x30, 0x7E, 0xA4, 0x53, 0x5A};
-uint8_t tagWork[5] = {0x30, 0x7E, 0xA1, 0x03, 0x92};
-uint8_t tagUCT[5] = {0x30, 0x7E, 0xA4, 0x53, 0x5A};
-uint8_t tagUCTJ[5] = {0x4C, 0x00, 0x84, 0xF3, 0xD8};
+uint8_t tag1[5] = {0x30, 0x7F, 0xA2, 0x53, 0x5A};
 
 uint16_t delay_us = 200;
 void setup()
@@ -20,7 +17,7 @@ void setup()
 void loop()
 {
   for(int i =0; i < 20; i++)
-    man.transmitEM4100(tagUCTJ);
+    man.transmitEM4100(tag1);
   man.SetDelay_us(delay_us++);
   if(delay_us > 220)
     delay_us = 200;
